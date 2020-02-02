cc94afd1f10e32f642ff6b8e399105e3074b669a
Riley Kuhlman <rilstarssun@gmail.com>
2020-02-02 14:47:09 -0600

    updated meta data

--- src/index.js
+++ src/index.js
@@ -21,18 +21,29 @@ class App extends Component {
               <MetaTags>
                 <title>Examples of Prospect Theory Applied</title>
                 <meta id="meta-description" name="description" content="Examples of prospect theory stretching from the first problems with utility theory and Allais all the way to modern decision making." />
-                <meta id="og-title" property="og:title" content="MyApp" />
-                <meta id="og-image" property="og:image" content="path/to/image.jpg" />
               </MetaTags>
               <Examples />
             </Route>
             <Route path="/explanation">
+            <MetaTags>
+              <title>Prospect Theory Explained</title>
+              <meta id="meta-description" name="description" content="A detailed explanation of prospect theory's history, the biases it tries to account for, and the math behind it all." />
+            </MetaTags>
+
               <Explanation />
-            </Route>
+            </Route> 
             <Route path="/calculator">
+              <MetaTags>
+                <title>Prospect Theory Calculator</title>
+                <meta id="meta-description" name="description" content="A cutting edge calculator capable of determining weighted probabilities and values for any number of choices." />
+              </MetaTags>
               <Calculator />
             </Route>
             <Route path="/">
+              <MetaTags>
+                <title>Prospect Theory</title>
+                <meta id="meta-description" name="description" content="Your home for calculations, explanations, and examples explaining prospect theory." />
+              </MetaTags>
               <Home />
             </Route>
           </Switch>
