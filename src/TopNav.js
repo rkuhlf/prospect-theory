49c97a4ccfe7aea3ae3929d27f164124300274ae
Riley Kuhlman <rilstarssun@gmail.com>
2020-04-15 12:40:48 -0500

    improved display slightly on mobile

--- src/TopNav.js
+++ src/TopNav.js
@@ -5,8 +5,8 @@ class TopNav extends Component {
 
   render() {
     return (
-      <div className="mb-2">
-        <nav className="navbar navbar-expand overflow-hidden navbar-dark bg-primary">
+      <div className="mb-2 scroll-horizontal-auto">
+        <nav className="navbar navbar-expand overflow-hidden scroll-horizontal-auto navbar-dark bg-primary">
           <Link className="navbar-brand" to="/">Prospect Theory</Link>
           <div className="navbar-nav">
             <Link className="nav-item nav-link" to="/explanation">Explanation</Link>
